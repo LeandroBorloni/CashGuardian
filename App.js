79b83bd20798136f5fcfef2e1d18996fb24b44c1
LeandroBorloni <lele6133@gmail.com>
2023-10-07 12:35:36 -0300

    login_app

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { TextInput } from 'react-native-web';
+import Button from './components/button';
+import Input from './components/input';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image style={styles.image} source={require('./assets/home.jpg')}></Image>
+      <Text>Sistema</Text>
+      <Text style={styles.title}>CashGuardian</Text>
+
+      <Input placeholder='e-mail'></Input>
+      <Input placeholder='senha' secureTextEntry></Input>
+
+      <Button>Entrar</Button>
+      <TouchableOpacity>
+        <Text>Criar conta</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FCFBFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color:'#5CAA60',
+    fontSize: 34,
+    fontWeight: 'bold',
+  },
+  image: {
+    height: 200,
+    width: '100%',
+    position: 'absolute',
+    top: 0,
+  }
 });
